2a598e20bb9373bf799b2ce823fcabcc1c68176b
Kaushal Kishor Sharma <kaushalkishor10702@gmail.com>
2023-08-27 22:19:52 +0530

    .

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,8 @@ import BlogList from "./bloglist";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null)
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch('http://localhost:8000/blogs')
@@ -12,11 +14,19 @@ const Home = () => {
       })
       .then(data => {
         setBlogs(data);
+        setIsPending(false);
+        setError(null);
+      })
+      .catch((err) => {
+        setIsPending(false);
+        setError(err.message);
       })
   }, []);
 
   return (
     <div className="home">
+      {error && <div> {error} </div>}
+      {isPending && <div> Loading.... </div>}
       {blogs && <BlogList blogs={blogs} />}
     </div>
   );
