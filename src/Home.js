831baf07b816348f779d30d8003a47849598af8d
Kaushal Kishor Sharma <kaushalkishor10702@gmail.com>
2023-08-28 19:35:42 +0530

    .

--- src/Home.js
+++ src/Home.js
@@ -1,28 +1,10 @@
 import { useState, useEffect } from "react";
 import BlogList from "./bloglist";
-
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null)
-  const [isPending, setIsPending] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    fetch('http://localhost:8000/blogs')
-      .then(res => {
-        return res.json();
-      })
-      .then(data => {
-        setBlogs(data);
-        setIsPending(false);
-        setError(null);
-      })
-      .catch((err) => {
-        setIsPending(false);
-        setError(err.message);
-      })
-  }, []);
-
+  const {data:blogs, isPending, error} = useFetch('http://localhost:8000/blogs');
+  
   return (
     <div className="home">
       {error && <div> {error} </div>}
